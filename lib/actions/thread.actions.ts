0ed2219da8d60a6b5d2bea4bac761bda507bc74b
Supriyo Saha <mm.saha1982@gmail.com>
2023-09-25 02:25:14 +0530

    post fetched in homepage

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,36 +1,68 @@
-"use server"
+"use server";
 
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connecToDB } from "../mongoose";
 
-
 interface Params {
-    text: string,
-    author: string,
-    communityId: string | null,
-    path: string,
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  try {
+    connecToDB();
+
+    const createdThread = await Thread.create({
+      text,
+      author,
+      commubity: null,
+    });
+
+    //update user model
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    });
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Failed to create thread: ${error.message}`);
   }
+}
 
-export async function createThread({text, author, communityId, path}: Params){
-    try {
-        connecToDB();
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connecToDB();
 
-        const createdThread = await Thread.create({
-            text,
-            author,
-            commubity: null,
-        });
+    //calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
 
-        //update user model
-        await User.findByIdAndUpdate(author, {
-            $push: { threads: createdThread._id}
-        })
+    //fetch posts that have no parents(top level threads...)
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined]}})
+    .sort({ createdAt : 'desc'})
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User})
+    .populate({
+        path: 'children',
+        populate: {
+            path: 'author',
+            model: User,
+            select: "_id name parentId image"
+        }
+    })
+    const totalPostsCount = await Thread.countDocuments({ parentId: {$in: [null, undefined]}})
 
-        revalidatePath(path);
+    const posts = await postsQuery.exec();
 
-    } catch (error: any) {
-        throw new Error(`Failed to create thread: ${error.message}`);
-    }
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext}
 }
