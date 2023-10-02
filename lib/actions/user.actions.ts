f4d92b556b82603b02b8bd89066463cb04e98304
Supriyo Saha <mm.saha1982@gmail.com>
2023-10-03 01:08:28 +0530

    search added

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connecToDB } from "../mongoose";
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
   userId: string;
@@ -53,27 +54,77 @@ export async function fetchUser(userId: string) {
   }
 }
 
-export async function fetchUserPosts(userId : string){
+export async function fetchUserPosts(userId: string) {
   try {
     connecToDB();
 
     //todo: community
-    const threads =  await User.findOne({ id: userId})
-    .populate({
-      path: 'threads',
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
       model: Thread,
       populate: {
-        path: 'children',
+        path: "children",
         model: "Thread",
         populate: {
           path: "author",
           model: User,
-          select: 'name image id'
-        }
-      }
-    })
-    return threads
+          select: "name image id",
+        },
+      },
+    });
+    return threads;
   } catch (error: any) {
-    throw new Error(`Faild to fetch user posts: ${error.message}`)
+    throw new Error(`Faild to fetch user posts: ${error.message}`);
   }
 }
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connecToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const regex = new RegExp(searchString, "i");
+
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId },
+    };
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+}
+
